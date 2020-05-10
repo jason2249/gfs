5da4609a09592bb576180080d50256b7680dab3f
Jason Lin <jason0@stanford.edu>
2020-05-09 18:21:04 -0700

    cache read metadata

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ class Client():
     def __init__(self):
         self.chunk_size = 64
         self.master_proxy = ServerProxy('http://localhost:9000')
+        self.read_cache = {} # (filename,chunk_idx) -> [chunk_id, [replica urls]]
 
     def create(self, filename):
         self.master_proxy.create(filename)
@@ -13,7 +14,13 @@ class Client():
     def read(self, filename, byte_offset, amount):
         chunk_idx = byte_offset // self.chunk_size
         chunk_offset = byte_offset % self.chunk_size
-        res = self.master_proxy.read(filename, chunk_idx)
+        if (filename, chunk_idx) in self.read_cache:
+            print("cached read")
+            res = self.read_cache[(filename, chunk_idx)]
+        else:
+            print("asking master for read")
+            res = self.master_proxy.read(filename, chunk_idx)
+            self.read_cache[(filename,chunk_idx)] = res
         chunk_id = res[0]
         replica_urls = res[1]
         replica_url = random.choice(replica_urls)
